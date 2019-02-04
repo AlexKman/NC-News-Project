c65909ce302aa2f9b06fadbde4aecaf1491768ae
AlexKman <keeley.lp@gmail.com>
2019-02-04 14:44:15 +0000

    early commit, links for pages articles, home and topics

---
+++ nc-news/src/views/Articles.js
@@ -0,0 +1,3 @@
+import React, { Component } from "react";
+
+export const Articles = () => <h1>Articles</h1>;
---
+++ nc-news/src/views/Topics.js
@@ -0,0 +1,3 @@
+import React, { Component } from "react";
+
+export const Topics = () => <h1>Topics</h1>;
