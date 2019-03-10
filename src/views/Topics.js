558cc22c2e1809e489e79975296272b5703c9b7b
AlexKMan <keeley.lp@gmail.com>
2019-03-10 21:54:33 +0000

    removed button styling

--- src/views/Topics.js
+++ src/views/Topics.js
@@ -3,7 +3,6 @@ import { Router, Link } from "@reach/router";
 import "../App.css";
 import { getTopics } from "../api";
 import "../css/topics.css";
-import Button from "react-bootstrap/Button";
 
 class Topics extends Component {
   state = {
@@ -25,16 +24,6 @@ class Topics extends Component {
             </Link>
           ))}
         </ul>
-
-        <Button variant="primary">Primary</Button>
-        <Button variant="secondary">Secondary</Button>
-        <Button variant="success">Success</Button>
-        <Button variant="warning">Warning</Button>
-        <Button variant="danger">Danger</Button>
-        <Button variant="info">Info</Button>
-        <Button variant="light">Light</Button>
-        <Button variant="dark">Dark</Button>
-        <Button variant="link">Link</Button>
       </div>
     );
   }
